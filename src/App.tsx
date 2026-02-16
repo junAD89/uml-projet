7a9ca0cbc78c259026ff21707db804c2ba7fa3da
junAD89 <balhighlight@gmail.com>
2026-02-16 06:55:55 +0000

    plan

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,9 @@ function App() {
           </section>
 
           {/* Title */}
-          <h1 className="text-4xl font-bold text-white mt-10">Welcome to Dashboard</h1>
+          <h1 className="text-4xl font-bold text-white mt-10">
+            Welcome to Dashboard
+          </h1>
         </div>
       </main>
     </div>
---
+++ src/pages/DemandeEmpruntPage.tsx
@@ -0,0 +1,7 @@
+export default function DemandeEmpruntPage() {
+    return (
+        <div>
+            <h1>Demendes d'emprunt</h1>
+        </div>
+    )
+}
