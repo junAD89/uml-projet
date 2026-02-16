da246a2d2ac040f3486aba3d1cd7fd92e9e2d99e
junAD89 <balhighlight@gmail.com>
2026-02-16 06:57:31 +0000

    s

--- src/pages/DashBoardPage.tsx
+++ src/pages/DashBoardPage.tsx
@@ -1,7 +1,43 @@
+import { ChevronRight, Search } from "lucide-react";
+
 export default function DashBoardPage() {
   return (
-    <div>
-      <h1>DashBoardPage</h1>
+    <div className="flex-1 p-6">
+      {/* Cards section */}
+      <div className="flex gap-6">
+        {/* Card: total emprunts */}
+        <div className="bg-[#1E1E1E] w-60 flex flex-col items-center justify-center p-6 rounded-[25px]">
+          <h3>nombre total d'emprunt</h3>
+          <div className="flex items-center gap-2">
+            <h1 className="text-3xl font-bold">34</h1>
+            <ChevronRight />
+          </div>
+        </div>
+        {/* Card: total livres */}
+        <div className="bg-[#1E1E1E] w-60 flex flex-col items-center justify-center p-6 rounded-[25px]">
+          <h3>Nombre total de livre</h3>
+          <div className="flex items-center gap-2">
+            <h1 className="text-3xl font-bold">84</h1>
+            <ChevronRight />
+          </div>
+        </div>
+      </div>
+
+      {/* Search section */}
+      <section className="mt-10">
+        <div className="flex items-center gap-3 bg-[#1e1e1e] p-4 rounded-lg">
+          <Search className="text-neutral-400" />
+          <input
+            className="bg-transparent outline-none flex-1 text-white placeholder-neutral-400"
+            placeholder="Search"
+          />
+        </div>
+      </section>
+
+      {/* Title */}
+      <h1 className="text-4xl font-bold text-white mt-10">
+        Welcome to Dashboard
+      </h1>
     </div>
   );
 }
